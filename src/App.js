f9b915fe41d1e2663e4fcb0b686198e272d2cc60
Achraf Khabar <95541956+Ashraf-Khabar@users.noreply.github.com>
2023-02-21 21:06:30 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import React from "react";
+import NavBar from "./components/navbar";
+import { useState } from "react";
 
 function App() {
+  const [isChecked, setIsChecked] = useState(false);
+  const [darkModeValue, setDarkModeValue] = useState('halloween');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <NavBar isChecked={isChecked} setDarkModeValue={setDarkModeValue} darkModeValue={darkModeValue} setIsChecked={setIsChecked}/>
+        <div className="container">
+          <Switch>
+            <Route>
+              
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </Router>
+
   );
 }
 
---
+++ src/components/test.jsx
@@ -0,0 +1,18 @@
+<div className="dropdown dropdown-end">
+    <label tabIndex={0} className="btn btn-ghost btn-circle">
+        <div className="indicator">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+            <span className="badge badge-sm indicator-item">8</span>
+        </div>
+    </label>
+    <div tabIndex={0} className="mt-3 card card-compact dropdown-content w-52 bg-base-100 shadow">
+        <div className="card-body">
+            <div className="form-control">
+                <input type="text" placeholder="Search" className="input input-bordered" />
+            </div>
+            <div className="card-actions">
+                <button className="btn btn-primary btn-block">Search</button>
+            </div>
+        </div>
+    </div>
+</div>
