a877c05eb028f31c61231cf90723a8a60382842e
Achraf Khabar <95541956+Ashraf-Khabar@users.noreply.github.com>
2023-02-21 23:25:35 +0100

    commit

--- src/components/home.js
+++ src/components/home.js
@@ -33,45 +33,117 @@ const Home = ({ isChecked, setIsChecked, darkModeValue, setDarkModeValue }) => {
                             <button className="btn btn-primary">See More</button>
                         </div>
                     </div>
-
                 </div>
-
             </section>
-
             <section>
-                <div data-theme={darkModeValue} className="artboard min-x-screen min-h-1 items-center justify-center flex">
-                    <div className="stats shadow " >
+                <div data-theme={darkModeValue} className="artboard min-x-screen min-h-fit items-center justify-center flex">
+                    <div className="stats shadow">
+
                         <div className="stat">
-                            <div className="stat-figure text-secondary">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+                            <div className="stat-figure text-primary">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path></svg>
                             </div>
-                            <div className="stat-title">Downloads</div>
-                            <div className="stat-value">31K</div>
-                            <div className="stat-desc">Jan 1st - Feb 1st</div>
+                            <div className="stat-title">Development skills</div>
+                            <div className="stat-value text-primary">60 %</div>
+                            <div className="stat-desc">Skills in Programming languages such as C/C++, java, python, php, java script</div>
                         </div>
 
                         <div className="stat">
                             <div className="stat-figure text-secondary">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4"></path></svg>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 10V3L4 14h7v7l9-11h-7z"></path></svg>
                             </div>
-                            <div className="stat-title">New Users</div>
-                            <div className="stat-value">4,200</div>
-                            <div className="stat-desc">↗︎ 400 (22%)</div>
+                            <div className="stat-title">Data Science - machine learning</div>
+                            <div className="stat-value text-secondary">30 %</div>
+                            <div className="stat-desc">Data science, data analysis, bigness intelligence, data base management, cloud computing ...</div>
+                        </div>
+
+                        <div className="stat">
+                            <div className="stat-figure text-sky-600">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 10V3L4 14h7v7l9-11h-7z"></path></svg>
+                            </div>
+                            <div className="stat-title">Marketing and management</div>
+                            <div className="stat-value text-sky-600">10 %</div>
+                            <div className="stat-desc">21% more than last month</div>
                         </div>
 
                         <div className="stat">
                             <div className="stat-figure text-secondary">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4"></path></svg>
+                                <div className="avatar online">
+                                    <div className="w-16 rounded-full">
+                                        <img src="https://avatars.githubusercontent.com/u/95541956?s=400&u=907244470dd4e5d4a047be77216b928ccfdac6dc&v=4" />
+                                    </div>
+                                </div>
                             </div>
-                            <div className="stat-title">New Registers</div>
-                            <div className="stat-value">1,200</div>
-                            <div className="stat-desc">↘︎ 90 (14%)</div>
+                            <div className="stat-value">Achraf khabar</div>
+                            <div className="stat-title">My skills</div>
+                            <div className="stat-desc text-secondary"></div>
                         </div>
 
                     </div>
                 </div>
+            </section>
+            <section>
+                <div data-theme={darkModeValue} className="artboard min-x-screen min-h-screen items-center justify-center flex">
+                    <div className="card w-96 bg-base-100 shadow-xl">
+                        <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+                        <div className="card-body">
+                            <h2 className="card-title">
+                                Shoes!
+                                <div className="badge badge-secondary">NEW</div>
+                            </h2>
+                            <p>If a dog chews shoes whose shoes does he choose?</p>
+                            <div className="card-actions justify-end">
+                                <div className="badge badge-outline">Fashion</div>
+                                <div className="badge badge-outline">Products</div>
+                            </div>
+                        </div>
+                    </div>
 
+                    <div className="card w-96 bg-base-100 shadow-xl">
+                        <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+                        <div className="card-body">
+                            <h2 className="card-title">
+                                Shoes!
+                                <div className="badge badge-secondary">NEW</div>
+                            </h2>
+                            <p>If a dog chews shoes whose shoes does he choose?</p>
+                            <div className="card-actions justify-end">
+                                <div className="badge badge-outline">Fashion</div>
+                                <div className="badge badge-outline">Products</div>
+                            </div>
+                        </div>
+                    </div>
 
+                    <div className="card w-96 bg-base-100 shadow-xl">
+                        <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+                        <div className="card-body">
+                            <h2 className="card-title">
+                                Shoes!
+                                <div className="badge badge-secondary">NEW</div>
+                            </h2>
+                            <p>If a dog chews shoes whose shoes does he choose?</p>
+                            <div className="card-actions justify-end">
+                                <div className="badge badge-outline">Fashion</div>
+                                <div className="badge badge-outline">Products</div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="card w-96 bg-base-100 shadow-xl">
+                        <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+                        <div className="card-body">
+                            <h2 className="card-title">
+                                Shoes!
+                                <div className="badge badge-secondary">NEW</div>
+                            </h2>
+                            <p>If a dog chews shoes whose shoes does he choose?</p>
+                            <div className="card-actions justify-end">
+                                <div className="badge badge-outline">Fashion</div>
+                                <div className="badge badge-outline">Products</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </section>
 
         </div>
