8dd6c60a96f487956591d0c3d7a61a364a4d0f22
Achraf Khabar <95541956+Ashraf-Khabar@users.noreply.github.com>
2023-04-15 11:52:09 +0000

    commit

--- src/components/stat.js
+++ src/components/stat.js
@@ -1,7 +1,40 @@
+import { useState } from "react";
 import React from "react";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Pie } from 'react-chartjs-2';
+
+ChartJS.register(ArcElement, Tooltip, Legend);
 
 const Stat = ({darkModeValue}) => {
-
+    const [pieData, setPieData] = useState({
+        labels: [],
+        datasets: [
+            {
+                label: [],
+                data: [
+                    
+                ],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)',
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)',
+                ],
+                borderWidth: 1
+            }
+        ]
+    });
+    
     return (
         <div data-theme={darkModeValue} className="artboard min-x-screen min-h-fit items-center justify-center flex">
             <div className="stats shadow">
